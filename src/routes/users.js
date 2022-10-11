41350d6c97ff8b8273502bd8010580dab8939af4
‘LinYuSir’ <linyusir@gmail.com>
2022-10-11 22:27:59 +0800

    redactor：调整目录结构

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,14 @@
+/*
+ * @LastEditors: Yx
+ * @LastEditTime: 2022-10-11 22:20:55
+ * @Description: 
+ * @Author: Yx
+ * @Date: 2022-10-11 21:58:06
+ * @FilePath: \node-koa2\src\routes\users.js
+ */
 const router = require('koa-router')()
 
+// 访问前缀
 router.prefix('/users')
 
 router.get('/', function (ctx, next) {
