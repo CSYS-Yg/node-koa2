44346be322608d976b41d0aabe57c3f92ed8857e
YGWork <linyusir@gmail.com>
2022-12-16 15:55:19 +0800

    feat：新增设置页面模板

--- src/routes/view/users.js
+++ src/routes/view/users.js
@@ -1,12 +1,13 @@
 /*
  * @LastEditors  : Yx
- * @LastEditTime : 2022-12-14 14:18:06
+ * @LastEditTime : 2022-12-16 15:50:14
  * @Description  : 用户页面 路由配置
  * @Author       : Yx
  * @Date         : 2022-11-03 09:09:02
  * @FilePath     : \node-koa2\src\routes\view\users.js
  */
 const router = require('koa-router')()
+const { loginRedirect } = require('../../middlewares/loginChecks')
 
 /**
  * 获取登录信息
@@ -33,6 +34,10 @@ router.get('/login', async (ctx, next) => {
 
 router.get('/register', async (ctx, next) => {
   await ctx.render('register', getLoginInfo(ctx))
+})
+
+router.get('/setting', loginRedirect, async (ctx, next) => {
+  await ctx.render('setting', ctx.session.userInfo)
 })
 
 module.exports = router
